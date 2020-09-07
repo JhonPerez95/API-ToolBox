6db9aa34d02211efad430067798b67f4add31658
Jhon Perez <jhonperez.j95@gmail.com>
2020-09-06 21:16:21 -0500

    add cors

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const app = require('./server');
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 4040;
 
 app.listen(port, () => {
   console.log(`Server started on ${port}`);
--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 
 // Initializations
 const app = express();
@@ -8,6 +9,7 @@ const app = express();
 
 // Config
 app.use(express.json());
+app.use(cors());
 
 // Routes
 app.use(require('./router/rootRoutes'));
