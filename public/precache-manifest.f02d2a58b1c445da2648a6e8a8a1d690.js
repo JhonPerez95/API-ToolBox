da00ea089e3ae867fe8cc81f36e83c2015cd7225
Jhon Perez <jhonperez.j95@gmail.com>
2020-09-06 22:07:25 -0500

    add app react

---
+++ public/precache-manifest.f02d2a58b1c445da2648a6e8a8a1d690.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0332a62200737de95446df922451477d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3577c6addd22f5c8d3e3",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "77cc977737cc8868980a",
+    "url": "/static/js/2.4a4d6f43.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4a4d6f43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3577c6addd22f5c8d3e3",
+    "url": "/static/js/main.7b68262b.chunk.js"
+  },
+  {
+    "revision": "0eafbac4c93c5b09ed1c",
+    "url": "/static/js/runtime-main.e4185f56.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
